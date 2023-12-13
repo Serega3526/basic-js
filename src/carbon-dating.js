57515284109f92ffa8b46ed2a34199e1539d4c27
Sergey Kruglov <serega-kolpino@inbox.ru>
2023-12-13 23:53:37 +0300

    feat: start and finish 1 task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  let k = 0.693 / HALF_LIFE_PERIOD
+    let result = Math.ceil((Math.log(MODERN_ACTIVITY / sampleActivity) / k))
+    if (typeof sampleActivity == 'string' && isFinite(result) && result > 0) {
+        return result
+    } else {
+        return false
+    }
 }
 
 module.exports = {
